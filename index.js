e6308be7981631de8ad62efbaf0a4a6f7f40936c
luizdominisini <dominizi.luiz@gmail.com>
2023-02-25 18:16:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 const prompt = require("prompt-sync")();
 
-impar = 0;
-pares = 0; 
+var impar = 0
+var pares = 0
 
-var n = parseInt(prompt("Digite um número inteiro:"));
-
-for(var num = n ; num <= 10 ; num++){
-  quadrado = num**2;
+for(num = 1 ; num <= 20 ; num++){
+  var quadrado = num ** 2;
   console.log(quadrado);
 
-  if(num % 2 == 0){
+  if(quadrado % 2 == 0){
     pares = pares + quadrado;
-  }  else{
+  } else {
     impar = impar + quadrado;
   }
 }
 
-console.log("A soma dos números pares é:",pares);
-console.log("A soma dos números ímpares é:",impar);
+console.log("Soma dos números pares: ",pares);
+console.log("Soma dos números ímpares: ",impar);
